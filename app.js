33a0c939e6e35689fcd93362579b1f074d44a78c
ArthurAsam <asa.arthur@icloud.com>
2024-02-18 21:13:34 -0600

    Add basic styling and JS event delegation
    Lines 37 through 57 add logic for button clicks
    which serve as the player selection and fire
    a single round and takes the input and runs
    a five round game.

--- app.js
+++ app.js
@@ -1,11 +1,65 @@
+function playGame(result){
+        let playerCounter = 0; 
+        let computerCounter = 0; 
+        for(let i = 1; i <= 5; i++){
+            //added getComputerChoice fn as a gn call rather than a variable to ensure it changes on each iteration
+            let result = callback(pSelectionLowerCase, getComputerChoice())
+            if( result === "That's a Tie!"){
+                console.log(result)
+                continue;  
+            }
+            else if (result === "You Win!"){
+                console.log(result)
+                ++playerCounter; 
+            } 
+            else ++computerCounter; 
+            console.log(result)
+        }
+        if(playerCounter === computerCounter){
+            return `Tie Game! ${playerCounter} | ${computerCounter}`
+        } else if (playerCounter > computerCounter){
+            return `Player wins! ${playerCounter} | ${computerCounter}`
+        } else 
+        return `Computer wins! ${computerCounter} | ${playerCounter}`
+    }
+    console.log(playGame(playSingleRound))
+
+
+
+
 function getComputerChoice(){
     let choices = ["rock", "paper", "scissors"]; 
     return choices[Math.floor(Math.random() * choices.length)]; 
 }
 
 
-let playerSelection = prompt("Choose: (rock)(paper) or (scissors)"); 
-let pSelectionLowerCase = playerSelection.toLowerCase(); 
+
+let outputDiv = document.getElementById('output'); 
+let container = document.querySelector('.btn-container')
+container.addEventListener('click', event => {
+    let playerId = event.target; 
+    let result = ''
+    switch(playerId.id){
+        case 'rock':
+            result = playSingleRound(playerId.id, getComputerChoice()); 
+               
+            
+            break; 
+        case 'paper':
+            result = playSingleRound(playerId.id, getComputerChoice()); 
+            
+            break; 
+        case 'scissors':
+            result = playSingleRound(playerId.id, getComputerChoice()); 
+            
+            break; 
+    }
+})
+
+
+
+
+
 
 function playSingleRound(player, computer){
     let result = ''; 
@@ -32,27 +86,3 @@ function playSingleRound(player, computer){
 
 
 
-function playGame(callback){
-    let playerCounter = 0; 
-    let computerCounter = 0; 
-    for(let i = 1; i <= 5; i++){
-        let result = callback(pSelectionLowerCase, getComputerChoice())
-        if( result === "That's a Tie!"){
-            console.log(result)
-            continue;  
-        }
-        else if (result === "You Win!"){
-            console.log(result)
-            ++playerCounter; 
-        } 
-        else ++computerCounter; 
-        console.log(result)
-    }
-    if(playerCounter === computerCounter){
-        return `Tie Game! ${playerCounter} | ${computerCounter}`
-    } else if (playerCounter > computerCounter){
-        return `Player wins! ${playerCounter} | ${computerCounter}`
-    } else 
-    return `Computer wins! ${computerCounter} | ${playerCounter}`
-}
-console.log(playGame(playSingleRound))
